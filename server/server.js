085d8829d6be36e6197aeecfefff4445adccf112
Joseph Caruana <jcaruana614@gmail.com>
2020-06-16 21:21:04 -0700

    server change

--- server/server.js
+++ server/server.js
@@ -1,20 +1,40 @@
+// importing modules
 const express = require('express');
 const app = express();
-
 const cors = require('cors');
-app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
-
 const cookieParser = require('cookie-parser');
-app.use(cookieParser());
+const bodyParser = require('body-parser');
+const path = require('path')
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 8000;
 
+// configuration
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(bodyParser.json());
 
-require('./config/mongoose.config');
+// files
 require('./models/user.model');
 require('./routes/user.routes')(app);
 
+mongoose.connect(process.env.MongoDB_URI || 'mongodb://localhost:27017/the_keep', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
 
-app.listen(8000, () => {
-    console.log("Listening at Port 8000")
+
+
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port: ${PORT}`);
 });
